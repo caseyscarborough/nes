4d222282847edcc9982689c5d87a18e4553e7d3d
Casey Scarborough <caseyscarborough@gmail.com>
2023-01-14 02:28:26 -0500

    Start cartridge implementation

--- src/cartridge.cpp
+++ src/cartridge.cpp
@@ -1,1 +1,24 @@
 #include "cartridge.h"
+
+bool Cartridge::load(std::string path)  {
+    LOG("Loading ROM from path " << path)
+    std::ifstream file(path, std::ios_base::binary | std::ios_base::in);
+    if (!file || !file.is_open()) {
+        LOG_ERROR("Could not open ROM file at path " << path)
+        return false;
+    }
+
+    Header header{};
+    if (!file.read(reinterpret_cast<char*>(&header), sizeof(header))) {
+        LOG_ERROR("Could not read iNES header");
+        return false;
+    }
+
+    if (header.constant[0] != 'N' || header.constant[1] != 'E' || header.constant[2] != 'S' || header.constant[3] != '\x1A') {
+        LOG_ERROR("iNES header is not in valid format. Possibly a different format?")
+        LOG_ERROR("Expected: 4e 45 53 1a")
+        LOG_ERROR("Got:      " << std::hex << unsigned(header.constant[0]) << " " << unsigned(header.constant[1]) << " " << unsigned(header.constant[2]) << " " << unsigned(header.constant[3]))
+        return false;
+    }
+    return true;
+}
--- src/cartridge.h
+++ src/cartridge.h
@@ -2,8 +2,27 @@
 #define NES_CARTRIDGE_H
 
 
+#include <string>
+#include <fstream>
+#include "log.h"
+
 class Cartridge {
 
+    // see https://www.nesdev.org/wiki/INES
+    struct Header {
+        char constant[4];     // ASCII "NES" followed by MS-DOS EOF
+        uint8_t prg_rom_size; // Size of PRG ROM in 16 KB units
+        uint8_t chr_rom_size; // Size of CHR ROM in 8 KB units
+        uint8_t flags6;       // Mapper, mirroring, battery, trainer
+        uint8_t flags7;       // Mapper, VS/Playchoice, NES 2.0
+        uint8_t flags8;       // PRG-RAM size (rarely used extension)
+        uint8_t flags9;       // TV system (rarely used extension)
+        uint8_t flags10;      // TV system, PRG-RAM presence (unofficial, rarely used extension)
+        uint8_t padding[5];   // Unused padding
+    };
+
+public:
+    bool load(std::string path);
 };
 
 
