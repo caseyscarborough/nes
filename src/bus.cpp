c56cfc90ae8649719d0f5c5ea7405fc04017cf5d
Casey Scarborough <caseyscarborough@gmail.com>
2023-01-19 18:55:22 -0500

    Improve reading/writing to components from the bus

--- src/bus.cpp
+++ src/bus.cpp
@@ -1,51 +1,38 @@
 #include "bus.h"
 #include "log.h"
 
-Bus::Bus() = default;
+
+Bus::Bus() {
+    ranges = {
+        {0x0000, 0x1FFF, AddressType::CPU,        &Bus::cpu_read,        &Bus::cpu_write},
+        {0x2000, 0x3FFF, AddressType::PPU,        &Bus::ppu_read,        &Bus::ppu_write},
+        {0x4015, 0x4015, AddressType::APU,        &Bus::apu_read,        &Bus::apu_write},
+        {0x4016, 0x4017, AddressType::Controller, &Bus::controller_read, &Bus::controller_write},
+        {0x8000, 0xFFFF, AddressType::Cartridge,  &Bus::cartridge_read,  &Bus::cartridge_write},
+    };
+}
 
 uint8_t Bus::read(uint16_t address) {
-    if (address < 0x2000) {
-        LOG_TRACE("Reading from memory address 0x" << std::hex << std::uppercase << address)
-        // The memory is mirrored every 0x07FF (2K) bytes.
-        return memory[address & 0x07FF];
-    } else if (address <= 0x4000) {
-        // PPU only has 8 registers (mirrored all the way through 0x3FFF).
-        return ppu->cpu_read(address & 0x2007);
-    } else if (address < 0x4018) {
-        // TODO: read from apu
-    } else if (address < 0x4020) {
-        // TODO: read from controller
-    } else {
-        LOG_TRACE("Reading from PRG-ROM at 0x" << std::hex << std::uppercase << address)
-        return this->cartridge->prg_read(address);
+    for (auto range: ranges) {
+        if (address >= range.start && address <= range.end) {
+            std::string name = get_address_type_name(range.type);
+            LOG_TRACE("Reading from " << name << " at address 0x" << std::hex << std::uppercase << address)
+            return (this->*range.read_callback)(address);
+        }
     }
-
     LOG_ERROR("Invalid read at address 0x" << std::hex << address)
     return 0x0;
 }
 
 void Bus::write(uint16_t address, uint8_t data) {
-    if (address < 0x2000) {
-        LOG_TRACE("Writing data to memory address 0x" << std::hex << std::uppercase << address)
-        // The memory is mirrored every 0x07FF (2K) bytes.
-        memory[address & 0x07FF] = data;
-        return;
-    } else if (address < 0x4000) {
-        // PPU has only 8 registers (mirrored all the way through 0x3FFF).
-        ppu->cpu_write(address & 0x2007, data);
-        return;
-    } else if (address < 0x4018) {
-        // TODO: write to apu
-        return;
-    } else if (address < 0x4020) {
-        // TODO: write to controller
-        return;
-    } else {
-        LOG_TRACE("Writing data to PRG-ROM at 0x" << std::hex << std::uppercase << address)
-        this->cartridge->prg_write(address, data);
-        return;
+    for (auto range: ranges) {
+        if (address >= range.start && address <= range.end) {
+            std::string name = get_address_type_name(range.type);
+            LOG_TRACE("Writing to " << name << " at address 0x" << std::hex << std::uppercase << address)
+            (this->*range.write_callback)(address, data);
+            return;
+        }
     }
-
     LOG_ERROR("Invalid write at address 0x" << std::hex << address)
 }
 
@@ -66,4 +53,57 @@ void Bus::load_cartridge(Cartridge *cartridge) {
 void Bus::connect_ppu(Ppu *ppu) {
     LOG_TRACE("Connecting PPU to the main bus...")
     this->ppu = ppu;
+}
+
+uint8_t Bus::cpu_read(uint16_t address) {
+    return memory[address];
+}
+
+void Bus::cpu_write(uint16_t address, uint8_t data) {
+    memory[address] = data;
+}
+
+uint8_t Bus::ppu_read(uint16_t address) {
+    return ppu->cpu_read(address);
+}
+
+void Bus::ppu_write(uint16_t address, uint8_t data) {
+    ppu->cpu_write(address, data);
+}
+
+uint8_t Bus::apu_read(uint16_t address) {
+    // TODO: Implement APU read
+    return 0;
+}
+
+void Bus::apu_write(uint16_t address, uint8_t data) {
+    // TODO: Implement APU write ?
+}
+
+uint8_t Bus::controller_read(uint16_t address) {
+    // TODO: Implement controller read
+    return 0;
+}
+
+void Bus::controller_write(uint16_t address, uint8_t data) {
+    // TODO: Implement controller write ?
+}
+
+uint8_t Bus::cartridge_read(uint16_t address) {
+    return cartridge->prg_read(address);
+}
+
+void Bus::cartridge_write(uint16_t address, uint8_t data) {
+    cartridge->prg_write(address, data);
+}
+
+std::string Bus::get_address_type_name(Bus::AddressType type) {
+    switch (type) {
+        case AddressType::CPU: return "CPU";
+        case AddressType::APU: return "APU";
+        case AddressType::PPU: return "PPU";
+        case AddressType::Controller: return "Controller";
+        case AddressType::Cartridge: return "Cartridge PRG-ROM";
+        default: return "Unknown";
+    }
 }
--- src/bus.h
+++ src/bus.h
@@ -3,8 +3,9 @@
 
 #include <cstdint>
 #include <cstdlib>
-#include "cpu.h"
+#include <vector>
 #include "cartridge.h"
+#include "cpu.h"
 #include "ppu.h"
 
 class Bus {
@@ -13,14 +14,42 @@ public:
     uint8_t read(uint16_t address);
     void write(uint16_t address, uint8_t data);
     void cycle();
-    void load_cartridge(Cartridge * cartridge);
-    void connect_cpu(Cpu* cpu);
-    void connect_ppu(Ppu* ppu);
+    void load_cartridge(Cartridge *cartridge);
+    void connect_cpu(Cpu *cpu);
+    void connect_ppu(Ppu *ppu);
 private:
+    enum class AddressType {
+        CPU,
+        PPU,
+        APU,
+        Controller,
+        Cartridge,
+    };
+    struct AddressRange {
+        uint16_t start;
+        uint16_t end;
+        AddressType type;
+        uint8_t (Bus::*read_callback)(uint16_t);
+        void (Bus::*write_callback)(uint16_t, uint8_t);
+    };
+
+    std::vector<AddressRange> ranges;
     Cpu *cpu;
     Ppu *ppu;
     Cartridge *cartridge;
-    uint8_t memory[8192];           // 0x0000 -> 0x07FF (mirrored 3 additional times)
+    uint8_t memory[8192];
+
+    uint8_t cpu_read(uint16_t address);
+    void cpu_write(uint16_t address, uint8_t data);
+    uint8_t ppu_read(uint16_t address);
+    void ppu_write(uint16_t address, uint8_t data);
+    uint8_t apu_read(uint16_t address);
+    void apu_write(uint16_t address, uint8_t data);
+    uint8_t controller_read(uint16_t address);
+    void controller_write(uint16_t address, uint8_t data);
+    uint8_t cartridge_read(uint16_t address);
+    void cartridge_write(uint16_t address, uint8_t data);
+    std::string get_address_type_name(AddressType type);
 };
 
 #endif //NES_BUS_H
