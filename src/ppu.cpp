6d0864f5768746e780072aba561c2cab6f0d2916
Casey Scarborough <caseyscarborough@gmail.com>
2023-01-22 15:51:42 -0500

    Begin PPU handling

--- src/ppu.cpp
+++ src/ppu.cpp
@@ -2,7 +2,18 @@
 
 // TODO: Implement read/write functions
 uint8_t Ppu::cpu_read(uint16_t address) {
-    return 0;
+    switch (address) {
+        case 0x2002: {
+            uint8_t data = status.get_value() & 0xE0; // mask the top three bits
+            status.clear(PpuStatus::Flag::VerticalBlank);
+            // TODO: Handle loopy register
+            return data;
+        }
+        case 0x2004: // TODO: Handle OAM
+        case 0x2007: // TODO: Handle PPUDATA
+        default:
+            return 0x0;
+    }
 }
 
 void Ppu::cpu_write(uint16_t address, uint8_t data) {
